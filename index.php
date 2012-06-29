d793059fd39947781a1c74adba6ca9993cd29712
Kalvin <ngkalvin123@gmail.com>
2012-06-29 18:05:00 -0400

    testing, first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+<h1>Pick Up Game Service Website</h1>
+
+<p>Please login to continue:</p>
+
+<form action="home.php" method="post">
+	Username: <input type="text" name="username" /><br />
+	Password: <input type="password" name="password" />
+	<input type="submit" value="Submit" />
+</form>
+
+<p> New user? <a href="register.php">Register</a> </p>
+
+</body>
+</html>
